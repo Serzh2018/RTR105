4b9c4e072d164310a462c16d4a765a872b60ffcb
Serzh2018 <42993005+Serzh2018@users.noreply.github.com>
2018-12-31 00:08:32 +0200

    history_20181218_14_00

---
+++ Teilora_Rindas_1LD_Gala rezultats_.py
@@ -0,0 +1,56 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /home/user/RTR105/Teilora_Rindas_1LD_Gala rezultats_.py ======
+exp(-x*x) aprekinasana: 
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 0.56
+exp(-x*x) (0.56) =   0.73
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =   0.73
+a500 =   0.00 S500 =   0.73
+exp(-x*x) (0.56) caur summu:   0.73
+
+ 
+
+                1000                              
+               ______                             
+               \               k      2*k         
+                \          (-1)   * x             
+     exp(-x*x) = >        _________________       
+                /                                 
+               /_____           k!                
+                 k=0                                    
+                 
+                 
+                                             2    
+                                    (-1) * x      
+     Rekurences reizinatajs:      ______________  
+     
+                                         k        
+>>> 
+===== RESTART: /home/user/RTR105/Teilora_Rindas_1LD_Gala rezultats_.py =====
+exp(-x*x) aprekinasana: 
+Lietotāj, lūdzu ievadi skaitli argumentu (x): 0.56
+exp(-x*x) (0.56) =   0.73
+a0 =   1.00 S0 =   1.00
+a499 =  -0.00 S499 =   0.73
+a500 =   0.00 S500 =   0.73
+exp(-x*x) (0.56) caur summu:   0.73
+
+                 1000                              
+               ______                             
+               \               k      2*k         
+                \          (-1)   * x             
+     exp(-x*x) = >        _________________       
+                /                                 
+               /_____           k!                
+                 k=0                              
+ 
+ 
+                                             2    
+                                    (-1) * x      
+     Rekurences reizinatajs:      ______________  
+                                                
+                                         k        
+>>> 

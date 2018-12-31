9f1409e5a16981f8de159d5a8606ed5984d72fdd
Serzh2018 <42993005+Serzh2018@users.noreply.github.com>
2018-12-31 13:10:46 +0200

    Delete dg17(3.LD).py

--- dg17(3.LD).py
+++
@@ -1,8 +0,0 @@
-Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
-[GCC 7.3.0] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
-================== RESTART: /home/user/RTR105/170.py ==================
-Dotajaa intervaalaa sakne(s) ir!
-Sakne ir:  3.1420654296875004
->>> 

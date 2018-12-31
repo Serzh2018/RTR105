c8c2fa4633322da167a5d0d2c22bbec9fa3af1fb
Serzh2018 <42993005+Serzh2018@users.noreply.github.com>
2018-12-31 19:50:10 +0200

    tests

---
+++ tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#http://www.cplusplus.com/reference/cstdio/printf/
+
+a = 10
+b = 1.54
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+a = 100500
+b = 988.78965
+c = 'A'
+print("a = %d"%(a))
+print("b = %.3f"%(b))
+print("c = %s"%(c))
